45f7bde8880665999544b66ddd91592ea1258632
Michael Zeevi <michael.52@msn.com>
2020-06-29 12:58:58 +0300

    reduced output to two columns, added output messages.

--- covid_normalizer.py
+++ covid_normalizer.py
@@ -5,14 +5,14 @@ import subprocess
 from pprint import pprint as print  # overwrite local print function
 
 # user args
+debug = False
 use_cached_data = True
 iso_code = "ISR"
-new_data_file = "data_output.csv"
+output_data_file = "data_output.csv"
 
 # config
 source_data_url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 raw_data_file = "/tmp/covid-data.csv"
-debug = False
 
 # converts a stringy float '3.14157' to an int 3, or an empty string to 0.
 def str_to_int(s):
@@ -23,6 +23,15 @@ def str_to_int(s):
 
 # main
 def main():
+  # debug print config
+  if debug:
+    print("DEBUG: debug = {}".format(debug))
+    print("DEBUG: use_cached_data = {}".format(use_cached_data))
+    print("DEBUG: raw_data_file = {}".format(raw_data_file))
+    print("DEBUG: iso_code = {}".format(iso_code))
+    print("DEBUG: source_data_url = {}".format(source_data_url))
+    print("DEBUG: output_data_file = {}".format(output_data_file))
+
   # check/get raw_data_file
   if use_cached_data and os.path.exists(raw_data_file):
     print("INFO: Using locally cached file '{}'!".format(raw_data_file))
@@ -37,6 +46,7 @@ def main():
 
   # populate lists
   with open(raw_data_file) as csvfile:
+    print("INFO: reading '{}'...".format(raw_data_file))
     csv_data = csv.reader(csvfile)
     for row in csv_data:
       if row[0] == iso_code:
@@ -46,19 +56,23 @@ def main():
           percent_new_cases = 0
         else:
           percent_new_cases = round(100 * new_cases/new_tests, 6)
-        processed_data.append([row[3], percent_new_cases, new_cases, new_tests])
-    column_names = ["date", "percent_new_cases", "new_cases", "new_tests"]
+        processed_data.append([row[3], percent_new_cases])
+    column_names = ["date", "percent_new_cases"]
 
-  # print data
+  # debug print data
   if debug:
+    print("DEBUG: printing processed data = ")
     print(processed_data)
 
   # populate new csv file
-  with open(new_data_file, 'w') as csvfile:
+  with open(output_data_file, 'w') as csvfile:
+    print("INFO: writing to '{}'...".format(output_data_file))
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(column_names)
     for row in processed_data:
       csvwriter.writerow(row)
+
+  print("Done! exiting...")
 
 # boilerplate
 if __name__ == "__main__":
